1ed04cf71e0da6db5183258c8cc62a7da2ccf3b1
azimut <azimut.github@protonmail.com>
2017-12-20 16:56:14 -0300

    trying to fix extempore

--- gentoo-bb/dock/kubler-spin/images/extempore/build.sh
+++ gentoo-bb/dock/kubler-spin/images/extempore/build.sh
@@ -2,10 +2,16 @@
 # build config
 #
 
-_packages="media-libs/alsa-lib
-           sys-devel/llvm:0/3.8.0"
+_packages="
+media-libs/alsa-lib
+media-libs/portaudio
+=media-libs/mesa-13.0.5
+x11-apps/mesa-progs
+x11-drivers/xf86-video-intel
+x11-libs/libXrandr
+x11-libs/libXinerama
+x11-libs/libXcursor"
 
-bdeps="x11-libs/libXrandr x11-libs/libXinerama x11-libs/libXcursor media-libs/mesa"
 # force native compilation
 _emerge_bin="emerge"
 
@@ -15,43 +21,53 @@ configure_bob(){
     # add extempore patch - mod to make it work with epatch
     # https://raw.githubusercontent.com/digego/extempore/master/extras/extempore-llvm-3.8.0.patch
     cp -vr /config/etc/portage/bashrc /etc/portage/
-    cp -vr /config/etc/portage/patches /etc/portage/env
+    cp -vr /config/etc/portage/patches /etc/portage/
 
     # mesa
-    echo 'VIDEO_CARDS="intel i915"' >> /etc/portage/make.conf
+    echo 'VIDEO_CARDS="intel i915 i965"' >> /etc/portage/make.conf
 
     # extempore uses llvm 3.8 - probably don't use clang
-    update_keywords 'sys-devel/llvm:0/3.8.0' '+~amd64'
-    update_keywords 'sys-devel/clang:0/3.8' '+~amd64'
-    update_use 'sys-devel/llvm' +clang -python
+    #update_keywords 'sys-devel/llvm:0/3.8.0' '+~amd64'
+    #update_keywords 'sys-devel/clang:0/3.8' '+~amd64'
+    #update_use 'sys-devel/llvm' +clang -python
+    update_use 'sys-devel/llvm' -clang -python
     update_use 'media-libs/alsa-lib' -python
 
-    emerge -1q dev-util/cmake ${_packages} ${bdeps}
+    update_keywords dev-util/cmake '+~amd64'
+    update_keywords sys-devel/llvm '+~amd64'
 
-    #rm -rf /distfiles/extempore2
-    #rsync --delete -av --exclude=cmake-build --exclude=.git /distfiles/extempore/ /distfiles/extempore2
+    update_use x11-base/xorg-server +glamor -udev
+    update_use media-libs/libepoxy +X
 
-    cd /distfiles/extempore2
-    sed -i 's/aot_extended ALL/aot_extended/g' CMakeLists.txt
-    sed -i 's/CMAKE_STATIC_LIBRARY_SUFFIX/CMAKE_SHARED_LIBRARY_SUFFIX/g' CMakeLists.txt
-    sed -i libs/external/gl.xtm -e 's/forced-gl-getprocaddress \#f/forced-gl-getprocaddress \#t/'
-    sed -i libs/external/glcompat.xtm -e 's/forced-gl-getprocaddress \#f/forced-gl-getprocaddress \#t/'
-    mkdir -p cmake-build/
-    cd cmake-build
-    make clean
-    emerge -1q sys-devel/gcc:5.4.0
-    gcc-config -l
-    gcc-config x86_64-pc-linux-gnu-5.4.0
-    export EXT_LLVM_DIR=/usr/
-    export EXTEMPORE_FORCE_GL_GETPROCADDRESS=1
-    cmake -DCMAKE_INSTALL_PREFIX=/opt ../
-    sed -i 's#../LLVM##g' CMakeFiles/extended_deps.dir/build.make
-    #make extempore aot_extended assets
-    make extempore
-    make extended_deps
-    make aot_extended
-    make install
-    #make DESTDIR="${pkgdir}/" install
+    # media-libs/portaudio media-libs/alsa-lib
+    #emerge --ignore-default-opts -q -b =dev-util/cmake-3.9.5 sys-devel/llvm:0/3.8.0
+    emerge -q =dev-util/cmake-3.9.5 sys-devel/llvm:0/3.8.0
+    emerge -q media-libs/portaudio media-libs/alsa-lib
+    emerge -q ${_packages}
+
+    # rm -rf /distfiles/extempore2
+    # rsync --delete -av --exclude=cmake-build --exclude=.git /distfiles/extempore/ /distfiles/extempore2
+
+    ### /distfiles/extempore2/cmake-build/extempore --nobase --noaudio --port=17085 --eval "(impc:aot:compile-xtm-file \"libs/external/audio_dsp_ext.xtm\")"
+
+    # cd /distfiles/extempore2
+    # sed -i 's/aot_extended ALL/aot_extended/g' CMakeLists.txt
+    # sed -i 's/CMAKE_STATIC_LIBRARY_SUFFIX/CMAKE_SHARED_LIBRARY_SUFFIX/g' CMakeLists.txt
+    # sed -i libs/external/gl.xtm -e 's/forced-gl-getprocaddress \#f/forced-gl-getprocaddress \#t/'
+    # sed -i libs/external/glcompat.xtm -e 's/forced-gl-getprocaddress \#f/forced-gl-getprocaddress \#t/'
+    # mkdir -p cmake-build/
+    # cd cmake-build
+    # make clean
+    # export EXT_LLVM_DIR=/usr/
+    # export EXTEMPORE_FORCE_GL_GETPROCADDRESS=1
+    # cmake -DCMAKE_INSTALL_PREFIX=/opt ../
+    # sed -i 's#../LLVM##g' CMakeFiles/extended_deps.dir/build.make
+    # #make extempore aot_extended assets
+    # make extempore
+    # make extended_deps # opengl
+    # LD_LIBRARY_PATH=/distfiles/extempore2/  make aot_extended
+    # make install
+    # #make DESTDIR="${pkgdir}/" install
 }
 
 configure_rootfs_build()
