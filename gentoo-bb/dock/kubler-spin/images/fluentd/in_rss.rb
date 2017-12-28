84e93b8d15363eab544e860a2a6e38341a03fe3b
azimut <azimut.github@protonmail.com>
2017-12-28 16:13:59 -0300

    rss parse comments and random sleep

--- gentoo-bb/dock/kubler-spin/images/fluentd/in_rss.rb
+++ gentoo-bb/dock/kubler-spin/images/fluentd/in_rss.rb
@@ -13,6 +13,7 @@ module Fluent::Plugin
     config_param :url, :string
     config_param :interval, :time, default: '5m'
     config_param :tagged, :string, default: 'generic'
+    config_param :parse_reddit_comments, :bool, default: false
 
     def configure(conf)
       super
@@ -31,7 +32,7 @@ module Fluent::Plugin
 
     def run
       loop do
-        sleep @interval
+        sleep @interval + Random.rand(@interval / 2 / 2)
         Thread.new(&method(:emit_rss))
       end
     end
@@ -60,6 +61,31 @@ module Fluent::Plugin
              record['published'] = item.published.strftime("%Y-%m-%d %H:%M:%S")
           end
           @router.emit(@tag, time, record)
+          if @parse_reddit_comments and record['url'] =~ /https:\/\/www.reddit.com.*/
+              sleep 10
+              rss = Feedjira::Feed.fetch_and_parse item.url + '.rss'
+              rss.entries.each do |item|
+                  record = {}
+                  record['title'] = item.title
+                  if item.content.nil? || item.content.empty?
+                    record['content'] = item.summary
+                  else
+                    record['content'] = item.content
+                  end
+                  record['feed'] = @url
+                  record['fingerprint'] = Digest::SHA256.hexdigest record['content']
+                  record['tags'] = @tagged
+                  record['url'] = item.url
+                  if item.published.nil?
+                     time = @current_time.to_i
+                     record['published'] = @current_time.strftime("%Y-%m-%d %H:%M:%S")
+                  else
+                     time = item.published.to_i
+                     record['published'] = item.published.strftime("%Y-%m-%d %H:%M:%S")
+                  end
+                  @router.emit(@tag + ".comments", time, record)
+              end
+          end
         end
       rescue => e
         log.error e
