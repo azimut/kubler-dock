2a6f5db0e09f51ad72f693572bc07adecc88d2d1
azimut <azimut.github@protonmail.com>
2018-04-05 19:08:07 -0400

    update pychess, add stockfish natively

--- dock/kubler-spin/images/pychess/build.sh
+++ dock/kubler-spin/images/pychess/build.sh
@@ -1,7 +1,7 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages="app-shells/bash
+_packages="games-board/stockfish::azimut
 games-board/pychess
 games-board/gnuchess
 games-board/gnuchess-book
@@ -12,6 +12,9 @@ games-board/crafty"
 # This hook is called just before starting the build of the root fs
 #
 configure_bob(){
+    # Stockfish
+    add_overlay azimut https://github.com/azimut/overlay
+    
     update_use dev-libs/glib +mime
     update_use x11-libs/gtk+ +X
     update_use x11-libs/cairo +X
@@ -21,22 +24,25 @@ configure_bob(){
     # https://bugs.gentoo.org/show_bug.cgi?id=611030
     # /usr/bin/gtk-query-immodules-3.0
     set +e
-    emerge -j25 -1vk --load-average=3 x11-libs/gtk+:3
+    emerge -1q x11-libs/gtk+:3
     set -e
 
-    # eclass bug that demands bash but isn't on RDEPEND
-    ROOT=${_EMERGE_ROOT} emerge -q1 app-shells/bash
-
-    local repo_url=https://github.com/ddugovic/Stockfish
-    local name=${repo_url##*/}
-    [[ ! -d /distfiles/${name} ]] && ( cd /distfiles; git clone ${repo_url}; )
-    [[   -d /distfiles/${name} ]] && ( cd /distfiles/${name}; git pull --rebase; )
-    # make build ARCH=x86-64
 }
 configure_rootfs_build()
 {
     unprovide_package dev-lang/python
-    update_use dev-lang/python +readline +ncurses
+    update_use 'dev-lang/python' +readline +ncurses
+     # add user
+    useradd --shell /bin/false \
+            --user-group \
+            --home-dir /home/user \
+            --create-home user
+    mkdir -p ${_EMERGE_ROOT}/home/user
+    chown -R user:user ${_EMERGE_ROOT}/home/user
+    # games.eclass - needs bash on ROOT otherwise
+    echo 'GAMES_USER=user' >> /etc/portage/make.conf
+    echo 'GAMES_USER_DED=user' >> /etc/portage/make.conf
+    echo 'games-board/* ~amd64' > /etc/portage/package.accept_keywords/games
 }
 
 #
@@ -47,20 +53,11 @@ finish_rootfs_build()
     # Stockfish
     copy_gcc_libs
 
-    sed -i 's#application/x-ms-dos-executable#application/x-sharedlib#g' \
-           ${_EMERGE_ROOT}/usr/${_LIB}/python2.7/site-packages/pychess/widgets/enginesDialog.py
-    #  filter.add_pattern("*.exe")   
-    #  filter.add_pattern("*")   
-
-    mkdir ${_EMERGE_ROOT}/engines
-    cp /distfiles/Stockfish/src/stockfish ${_EMERGE_ROOT}/engines/
-    strip ${_EMERGE_ROOT}/engines/stockfish
-
     # https://github.com/docker-library/python/blob/master/3.4/alpine/Dockerfile
-	find ${_EMERGE_ROOT}/ -depth \
-			\( \
-				\( -type d -a -name test -o -name tests -o -name testing \) \
-				-o \
-				\( -type f -a -name '*.pyo' -o -name '*.pyc' \) \
-	        \) -print -exec rm -rf '{}' + 
+    find ${_EMERGE_ROOT}/ -depth \
+	 \( \
+	 \( -type d -a -name test -o -name tests -o -name testing \) \
+	 -o \
+	 \( -type f -a -name '*.pyo' -o -name '*.pyc' \) \
+	 \) -print -exec rm -rf '{}' + 
 }
